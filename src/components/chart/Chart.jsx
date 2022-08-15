25f8441a2d3cdb8e091165fa6cbc239a27f04641
“cloudmate404” <ogunniyi95@gmail.com>
2022-08-15 22:07:12 +0100

    2nd commit

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,6 +1,58 @@
+// react-vis /from uber
+// line charts / from nivo.rocks
+// recharts /recharts
+// react-chartjs
+
 import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
 import "./chart.scss";
 
+const data = [
+  { name: "January", Total: 12000 },
+  { name: "February", Total: 11000 },
+  { name: "March", Total: 9000 },
+  { name: "April", Total: 14000 },
+  { name: "May", Total: 13000 },
+  { name: "June", Total: 10000 },
+];
 export const Chart = () => {
-  return <div className="chart">Chart</div>;
+  return (
+    <div className="chart">
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1.2}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,6 +3,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 export const Featured = () => {
   return (
     <div className="featured">
@@ -22,8 +24,23 @@ export const Featured = () => {
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
-            <div className="itemResult">
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
               <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$3k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$10k</div>
             </div>
           </div>
         </div>
